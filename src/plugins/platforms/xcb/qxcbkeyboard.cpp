lunascape-corporation/qtbase

Added Tizen hardware button handling.
It is mainly the same as in the Android so Android solution
 was adapted.

Change-Id: Ie86c5fdd6e25178054e4df77ec4fe318960461ba
Reviewed-by: Tomasz Olszak <olszak.tomasz@gmail.com>

src/plugins/platforms/xcb/qxcbkeyboard.cpp
@@ -250,6 +250,10 @@
 #define XF86XK_TouchpadOff         0x1008FFB1
 #define XF86XK_AudioMicMute        0x1008FFB2
 
+#ifdef Q_OS_TIZEN
+#define TIZEN_Key_Back             XF86XK_Stop
+#define TIZEN_Key_Menu             XF86XK_Send
+#endif //Q_OS_TIZEN
 
 // end of XF86keysyms.h
 
@@ -426,7 +430,11 @@ static const unsigned int KeyTbl[] = {
         // wireless/bluetooth/uwb keys, special launcher keys, etc.
     XF86XK_Back,                Qt::Key_Back,
     XF86XK_Forward,             Qt::Key_Forward,
+#ifdef Q_OS_TIZEN
+    TIZEN_Key_Back,             Qt::Key_Back,
+#else
     XF86XK_Stop,                Qt::Key_Stop,
+#endif //Q_OS_TIZEN
     XF86XK_Refresh,             Qt::Key_Refresh,
     XF86XK_Favorites,           Qt::Key_Favorites,
     XF86XK_AudioMedia,          Qt::Key_LaunchMedia,
@@ -506,7 +514,11 @@ static const unsigned int KeyTbl[] = {
     XF86XK_RotationPB,          Qt::Key_RotationPB,
     XF86XK_RotationKB,          Qt::Key_RotationKB,
     XF86XK_Save,                Qt::Key_Save,
+#ifdef Q_OS_TIZEN
+    TIZEN_Key_Menu,             Qt::Key_Menu,
+#else
     XF86XK_Send,                Qt::Key_Send,
+#endif //Q_OS_TIZEN
     XF86XK_Spell,               Qt::Key_Spell,
     XF86XK_SplitScreen,         Qt::Key_SplitScreen,
     XF86XK_Support,             Qt::Key_Support,
@@ -561,7 +573,6 @@ static const unsigned int KeyTbl[] = {
     XF86XK_LaunchD,             Qt::Key_LaunchF,
     XF86XK_LaunchE,             Qt::Key_LaunchG,
     XF86XK_LaunchF,             Qt::Key_LaunchH,
-
     0,                          0
 };
 
@@ -1259,6 +1270,13 @@ void QXcbKeyboard::handleKeyEvent(QWindow *window, QEvent::Type type, xcb_keycod
     // It is crucial the order of xkb_state_key_get_one_sym &
     // xkb_state_update_key operations is not reversed!
     xcb_keysym_t sym = xkb_state_key_get_one_sym(xkb_state, code);
+#ifdef Q_OS_TIZEN
+    if (type == QEvent::KeyPress && (sym == TIZEN_Key_Back || sym == TIZEN_Key_Menu)) {
+        //silently omit press events for Back and Menu keys
+        return;
+    }
+#endif //Q_OS_TIZEN
+
 #ifdef QT_NO_XKB
     enum xkb_key_direction direction;
     if (type == QEvent::KeyPress)

tizenorg/platform.upstream.qtbase

src/plugins/platforms/xcb/qxcbkeyboard.cpp
@@ -252,6 +252,10 @@
 #define XF86XK_TouchpadOff         0x1008FFB1
 #define XF86XK_AudioMicMute        0x1008FFB2
 
+#ifdef Q_OS_LINUX_TIZEN
+#define TIZEN_Key_Back             XF86XK_Stop
+#define TIZEN_Key_Menu             XF86XK_Send
+#endif //Q_OS_LINUX_TIZEN
 
 // end of XF86keysyms.h
 
@@ -428,7 +432,11 @@ static const unsigned int KeyTbl[] = {
         // wireless/bluetooth/uwb keys, special launcher keys, etc.
     XF86XK_Back,                Qt::Key_Back,
     XF86XK_Forward,             Qt::Key_Forward,
+#ifdef Q_OS_LINUX_TIZEN
+    TIZEN_Key_Back,             Qt::Key_Back,
+#else
     XF86XK_Stop,                Qt::Key_Stop,
+#endif //Q_OS_LINUX_TIZEN
     XF86XK_Refresh,             Qt::Key_Refresh,
     XF86XK_Favorites,           Qt::Key_Favorites,
     XF86XK_AudioMedia,          Qt::Key_LaunchMedia,
@@ -508,7 +516,11 @@ static const unsigned int KeyTbl[] = {
     XF86XK_RotationPB,          Qt::Key_RotationPB,
     XF86XK_RotationKB,          Qt::Key_RotationKB,
     XF86XK_Save,                Qt::Key_Save,
+#ifdef Q_OS_LINUX_TIZEN
+    TIZEN_Key_Menu,             Qt::Key_Menu,
+#else
     XF86XK_Send,                Qt::Key_Send,
+#endif //Q_OS_LINUX_TIZEN
     XF86XK_Spell,               Qt::Key_Spell,
     XF86XK_SplitScreen,         Qt::Key_SplitScreen,
     XF86XK_Support,             Qt::Key_Support,
@@ -567,7 +579,6 @@ static const unsigned int KeyTbl[] = {
     XF86XK_LaunchD,             Qt::Key_LaunchF,
     XF86XK_LaunchE,             Qt::Key_LaunchG,
     XF86XK_LaunchF,             Qt::Key_LaunchH,
-
     0,                          0
 };
 
@@ -1296,6 +1307,12 @@ void QXcbKeyboard::handleKeyEvent(QWindow *window, QEvent::Type type, xcb_keycod
 
     // It is crucial the order of xkb_state_key_get_one_sym & xkb_state_update_key operations is not reversed!
     xcb_keysym_t sym = xkb_state_key_get_one_sym(xkb_state, code);
+#ifdef Q_OS_LINUX_TIZEN
+    if (type == QEvent::KeyPress && (sym == TIZEN_Key_Back || sym == TIZEN_Key_Menu)) {
+        //silently omit press events for Back and Menu keys
+        return;
+    }
+#endif //Q_OS_LINUX_TIZEN
 
     QPlatformInputContext *inputContext = QGuiApplicationPrivate::platformIntegration()->inputContext();
     QMetaMethod method;