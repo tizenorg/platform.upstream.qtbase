lunascape-corporation/qtbase

Tizen - handling application lifecycle
Change-Id: I47763f2d42e400df3e0e60515468ada8c8546128
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
Reviewed-by: Tomasz Olszak <olszak.tomasz@gmail.com>

src/plugins/platforms/xcb/qxcbconnection.h
@@ -270,6 +270,9 @@ namespace QXcbAtom {
 #if XCB_USE_MAEMO_WINDOW_PROPERTIES
         MeegoTouchOrientationAngle,
 #endif
+#ifdef Q_OS_TIZEN
+        _X_ILLUME_DEACTIVATE_WINDOW,
+#endif
         _XSETTINGS_SETTINGS,
 
         NPredefinedAtoms,

tizenorg/platform.upstream.qtbase

src/plugins/platforms/xcb/qxcbconnection.h
@@ -273,6 +273,9 @@ namespace QXcbAtom {
         RelHorizScroll,
         RelVertScroll,
 
+#ifdef Q_OS_LINUX_TIZEN
+        _X_ILLUME_DEACTIVATE_WINDOW,
+#endif
         _XSETTINGS_SETTINGS,
 
         _COMPIZ_DECOR_PENDING,